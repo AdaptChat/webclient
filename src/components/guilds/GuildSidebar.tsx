50b5681b721d2facb6a4c5087cfc2ec72d6a5b1d
jay3332 <40323796+jay3332@users.noreply.github.com>
2023-02-15 22:25:37 -0500

    add support for banners

--- src/components/guilds/GuildSidebar.tsx
+++ src/components/guilds/GuildSidebar.tsx
@@ -17,13 +17,24 @@ export default function GuildSidebar() {
 
   return (
     <div class="flex flex-col items-center justify-center w-full">
-      <div class="w-56 mt-4 card flex border border-2 border-base-content/10">
+      <div class="w-[calc(100%-2rem)] mt-4 card flex border border-2 border-base-content/10">
         {guild.banner && (
           <figure class="h-20 overflow-hidden flex items-center justify-center">
             <img src={guild.banner} alt="" class="w-full" width="100%" />
           </figure>
         )}
-        <div class="flex justify-center font-title card-title text-center py-2">{guild.name}</div>
+        <div classList={{
+          "flex justify-between px-4 pt-2": true,
+          "pb-2": !guild.description,
+        }}>
+          <span class="font-title card-title text-base">{guild.name}</span>
+
+        </div>
+        {guild.description && (
+          <div class="card-body px-4 pt-1 pb-2">
+            <p class="text-xs text-base-content/50">{guild.description}</p>
+          </div>
+        )}
       </div>
       <div class="flex flex-col w-full p-2">
         <SidebarButton href={`/guilds/${guildId}`} svg="/icons/home.svg" active={!channelId()}>Home</SidebarButton>
