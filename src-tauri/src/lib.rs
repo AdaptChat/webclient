ef43e24102ad8a1f436b50570a0d9789d65916f4
Cryptex-github <64497526+Cryptex-github@users.noreply.github.com>
2024-10-03 19:29:52 -0700

    Begin migrating to tauri v2

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,9 @@
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_dialog::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  preview: {
+    port: 3000,
+  },
   build: {
     target: 'esnext',
   },
