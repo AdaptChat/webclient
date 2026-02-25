058e737c974d1d15bcb2adda718d5e72dceae88d
jay3332 <40323796+jay3332@users.noreply.github.com>
2026-02-25 03:05:06 -0500

    migrate to tauri v2

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,10 @@
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
